549f46077e4baa771f0e6e2e2a27fc086a8563f8
vigor-lundkvist <vigor.lundkvist@live.se>
2021-11-08 13:03:31 +0100

    Displays DB information in Listbox.

---
+++ ContactData/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var MyContacts = new PeopleEntities().People;
+
+            foreach (var item in MyContacts)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //ListBox Persons = sender as ListBox;
+            //People SelectedPerson = Persons.SelectedItem as People;
+            //MessageBox.Show("Test");
+        }
+    }
+    public partial class People
+    {
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
